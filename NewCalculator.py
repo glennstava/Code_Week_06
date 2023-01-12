5097af213475f493033609f997e88bba359f8a15
Glenn Stava <glenn.sava@gmail.com>
2023-01-12 10:28:39 +0100

    Extra math functions added

--- NewCalculator.py
+++ NewCalculator.py
@@ -3,3 +3,11 @@
 def addingNumbers(noOne, noTwo):
     print(f"{noOne} + {noTwo} equals {noOne + noTwo}")
 
+def subtractingNumbers(noOne, noTwo):
+    print(f"{noOne} - {noTwo} equals {noOne - noTwo}")
+
+def multiplyingNumbers(noOne, noTwo):
+    print(f"{noOne} times {noTwo} equals {noOne * noTwo}")
+
+def dividingNumbers(noOne, noTwo):
+    print(f"{noOne} divided by {noTwo} equals {noOne / noTwo}")
