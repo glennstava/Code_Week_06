532fe6917fb03f3faca80aee1659b397c5f69c40
Glenn Stava <glenn.sava@gmail.com>
2023-01-12 10:39:02 +0100

    Negative message added

--- NewCalculator.py
+++ NewCalculator.py
@@ -4,7 +4,12 @@ def addingNumbers(noOne, noTwo):
     print(f"{noOne} + {noTwo} equals {noOne + noTwo}")
 
 def subtractingNumbers(noOne, noTwo):
-    print(f"{noOne} - {noTwo} equals {noOne - noTwo}")
+    subtractionSum = noOne - noTwo
+
+    if subtractionSum < 0:
+        print("Answer is a negative number")
+    else:
+        print(f"{noOne} - {noTwo} equals {subtractionSum}")
 
 def multiplyingNumbers(noOne, noTwo):
     print(f"{noOne} times {noTwo} equals {noOne * noTwo}")
